273c3c70d6f231ab00ea2d502440f8ba4ea6da63
vika <37137412+hi-im-vika@users.noreply.github.com>
2026-03-22 16:59:56 -0700

    Add example dpg code

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import dearpygui.dearpygui as dpg
+
 def main():
     print("Hello from ctrly-py!")
+    dpg.create_context()
+    dpg.create_viewport(title='Custom Title', width=600, height=300)
+
+    with dpg.window(label="Example Window"):
+        dpg.add_text("Hello, world")
+        dpg.add_button(label="Save")
+        dpg.add_input_text(label="string", default_value="Quick brown fox")
+        dpg.add_slider_float(label="float", default_value=0.273, max_value=1)
+
+    dpg.setup_dearpygui()
+    dpg.show_viewport()
+    dpg.start_dearpygui()
+    dpg.destroy_context()
 
 
 if __name__ == "__main__":
