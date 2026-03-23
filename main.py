5a7e15eca133c42866c22d6ecc6906da8221443c
vika <37137412+hi-im-vika@users.noreply.github.com>
2026-03-22 19:16:30 -0700

    Write function to find serial port (lowest ttyACMX device where X != 0)

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import serial, re
+from serial.tools import list_ports
+
+def find_port():
+    ports = list_ports.grep(r'^\/dev\/ttyACM[1-9]+')    # ignore ttyACM0
+    try:
+        port = next(ports)
+        return port
+    except StopIteration:
+        raise Exception("No ports found")   
+
 def main():
     print("Hello from ctrly-py!")
 
