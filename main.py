194de063fde381cac1528875f024b11222db60f8
vika <37137412+hi-im-vika@users.noreply.github.com>
2026-03-22 19:34:23 -0700

    Define serial thread function

--- main.py
+++ main.py
@@ -8,7 +8,31 @@ def find_port():
         port = next(ports)
         return port
     except StopIteration:
-        raise Exception("No ports found")   
+        raise Exception("No ports found")
+
+def serial_thread():
+    port = ''
+    while True:
+        while not port:
+            try:
+                port = find_port()
+            except:
+                print("No port found. Retrying in 1s")
+                time.sleep(1)
+        try:
+            print(port.device)
+            with serial.Serial(port.device, baudrate=115200) as ser:
+                while True:
+                    # put TX/RX stuff here
+                    print(f"Serial port found ({port.device})")
+                    ser.write(b'hello')
+                    time.sleep(1)
+        except Exception as e:
+            print(e)
+            print("Serial port error. Disconnecting and retrying")
+            ser.close()
+            port = ''
+            time.sleep(1)     
 
 def main():
     print("Hello from ctrly-py!")
