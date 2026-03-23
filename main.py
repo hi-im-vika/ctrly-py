907f3997966325575a6acc18d1b44d074eefa118
vika <37137412+hi-im-vika@users.noreply.github.com>
2026-03-22 17:35:16 -0700

    Add placeholder widgets

--- main.py
+++ main.py
@@ -3,13 +3,25 @@ import dearpygui.dearpygui as dpg
 def main():
     print("Hello from ctrly-py!")
     dpg.create_context()
-    dpg.create_viewport(title='Custom Title', width=600, height=300)
+    dpg.create_viewport(title='Custom Title')
 
-    with dpg.window(label="Example Window"):
+    with dpg.window(label="The Window",tag="Primary Window"):
         dpg.add_text("Hello, world")
-        dpg.add_button(label="Save")
-        dpg.add_input_text(label="string", default_value="Quick brown fox")
-        dpg.add_slider_float(label="float", default_value=0.273, max_value=1)
+        dpg.add_slider_int(label="Throttle", vertical=True, max_value=100, height=160)
+        dpg.add_slider_int(label="Steering", vertical=True, max_value=100, height=160)
+        with dpg.table(header_row=False):
+
+            # use add_table_column to add columns to the table,
+            # table columns use slot 0
+            dpg.add_table_column()
+            dpg.add_table_column()
+
+            with dpg.table_row():
+                dpg.add_text(f"Refresh rate")
+                dpg.add_text(f"9000 hz")
+            with dpg.table_row():
+                dpg.add_text(f"Response time")
+                dpg.add_text(f"0.1 ms")
 
     dpg.setup_dearpygui()
     dpg.show_viewport()
